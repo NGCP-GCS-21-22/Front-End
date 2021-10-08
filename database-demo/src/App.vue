6aa0d31b853a28da5b8f60c27147c2daf8f9e272
Phuong Nguyen <phuongnpa1995@gmail.com>
2021-10-08 13:02:59 -0700

    Update UI

---
+++ database-demo/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <form action="">
+    <label for="">Vehicle Input:</label>
+    
+    <p><input type="text" v-model="vehicle_name"> {{vehicle_name}}</p>
+    <button @click="post_vehicle_type">Submit</button>
+  </form>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data()
+  { 
+      return{
+        vehicle_name: "UAV"
+      }
+  },
+  methods:{
+    post_vehicle_type(){
+      const path = "http://localhost:3000/data"
+      axios.get(path)
+      .then
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
