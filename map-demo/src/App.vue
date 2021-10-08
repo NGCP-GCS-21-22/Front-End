9afd4275c33d0c8497918533b2523206fdb9cc94
matthewkhle <mattmattmattle@gmail.com>
2021-10-08 15:14:51 -0700

    update map

--- map-demo/src/App.vue
+++ map-demo/src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <GMapMap
-      :center="{lat: 51.093048, lng: 6.842120}"
-      :zoom="7"
-      map-type-id="terrain"
-      style="width: 100vw; height: 900px"
+    :center="center"
+    :zoom="7"
+    map-type-id="terrain"
+    style="width: 100vw; height: 900px"
   >
+    <GMapMarker icon="true" :key="index" v-for="(m, index) in markers" />
   </GMapMap>
 </template>
 
 <script >
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
-      center: {lat: 51.093048, lng: 6.842120},
-    }
-  }
-}
+      center: { lat: 51.093048, lng: 6.84212 },
+      markers: [
+        {
+          position: {
+            lat: 51.093048,
+            lng: 6.84212,
+          },
+        },
+      ],
+    };
+  },
+};
 </script>
--- map-demo/src/main.js
+++ map-demo/src/main.js
@@ -5,12 +5,6 @@ import VueGoogleMaps from '@fawmi/vue-google-maps'
 const app = createApp(App);
 app.use(VueGoogleMaps, {
     load: {
-        key: 'AIzaSyALxRDktZaYsxXCYJQjLdJ6Wz-l3paEBag',
+        key: 'AIzaSyDmpjAhZm5MuAHTMu73O4jvdydifGtSNrM',
     },
 }).mount('#app')
-app.use(VueGoogleMaps, {
-    load: {
-        key: 'AIzaSyALxRDktZaYsxXCYJQjLdJ6Wz-l3paEBag',
-    },
-    autobindAllEvents: true,
-})
