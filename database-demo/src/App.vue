16c54aa6f26743167659491f4aeac98c58019dc9
Phuong Nguyen <phuongnpa1995@gmail.com>
2021-10-08 13:51:24 -0700

    Done with post

--- database-demo/src/App.vue
+++ database-demo/src/App.vue
@@ -1,32 +1,51 @@
 <template>
-  <form action="">
+  
     <label for="">Vehicle Input:</label>
-    
-    <p><input type="text" v-model="vehicle_name"> {{vehicle_name}}</p>
+
+    <p><input type="text" v-model="vehicle" /> {{ vehicle }}</p>
     <button @click="post_vehicle_type">Submit</button>
-  </form>
+
 </template>
 
 <script>
-
-
+import axios from "axios";
 export default {
-  name: 'App',
-  data()
-  { 
-      return{
-        vehicle_name: "UAV"
-      }
+  name: "App",
+  data() {
+    return {
+      vehicle: "UAV",
+    };
   },
-  methods:{
-    post_vehicle_type(){
-      const path = "http://localhost:3000/data"
-      axios.get(path)
-      .then
-    }
-  }
-  
-}
+  methods: {
+    // get_vehicle_type(){
+    //    const path = 'http://localhost:3000/data';
+    //   axios.get(path)
+    //     .then((res) => {
+    //       this.vehicle = res.data.vehicle;
+    //     })
+    //     .catch((error) => {
+    //       // eslint-disable-next-line
+    //       console.error(error);
+    //     });
+    // },
+    post_vehicle_type() {
+      // const vehicle_name = data();
+      const vehicle_name = JSON.stringify({
+        vehicle: this.vehicle,
+      });
+      console.log(this.vehicle);
+      const path = "http://localhost:3000/data";
+      axios
+        .post(path, vehicle_name)
+        .then(() => {
+          console.log("Sucessfully Post!");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
