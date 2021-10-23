5fb7058d32d4bdb81b5966ed7039e4d425600b51
Chelsea Leung <leungchelsea2018@gmail.com>
2021-10-23 13:09:15 -0700

    Update map marker

--- google-maps-demo/src/App.vue
+++ google-maps-demo/src/App.vue
@@ -7,9 +7,17 @@
       style="width: 100vw; height: 100vh"
       :tilt="tilt"
       :options="options"
+      @click="handleMapClick"
     >
-    <!-- stuff here maybe? marker?? -->
+    <GmapMarker
+      :position="marker.position"
+      :clickable="true"
+      :draggable="true"
+      @drag="handleMarkerDrag"
+      @click="panToMarker"
+    />
     </GmapMap>
+    <p>Selected Position: {{ marker.position }}</p>
   </div>
 </template>
 
@@ -19,6 +27,7 @@ export default {
   data() {
     return {
       // Map Data
+      marker: { position: {lat: 33.93364332758927, lng: -117.63119269704840} },
       center: { lat: 33.93364332758927, lng: -117.6311926970484 },
       zoom: 18,
       tilt: 0,
@@ -33,6 +42,19 @@ export default {
       mapType: "satellite",
     };
   },
+  methods: {
+    handleMarkerDrag(e) {
+      this.marker.position = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+    },
+    panToMarker() {
+      this.$refs.mapRef.panTo(this.marker.position);
+      this.$refs.mapRef.setZoom(18);
+    },
+    handleMapClick(e) {
+      this.marker.position = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+      console.log(e);
+    },
+  }
 };
 </script>
 
