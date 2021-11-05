e2255821ea2ae956e929dc9dfff17982ff274a33
matthewkhle <mattmattmattle@gmail.com>
2021-11-05 11:51:08 -0700

    Removed old projects and merged vehicle status components

---
+++ front-end/src/components/Main/VehicleStatus.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <b-container>
+      <b-card style="width:550px; height:190px">
+        <b-row>
+          <b-col>
+            <h2 align="left">MAC Status</h2>
+          </b-col>
+          <b-col cols="1">
+            <b-img
+              :src="require('@/assets/airplane.png')"
+              style="width:60px; height:50px"
+              >
+            </b-img>
+          </b-col>    
+          <b-col>
+            Connection: Online
+            </br>
+            Last Packet: 2ms
+          </b-col>
+            <b-icon icon="reception4" style="color: #29C16E; width:50px; height:50px"></b-icon>
+          
+        </b-row>
+        
+        <b-row>
+        <b-card-subtitle align="left">Mission Stage: Return to Base</b-card-subtitle>
+        </b-row>
+        <b-row>
+            <b-col cols="7">
+              <b-icon icon="battery-full" style="color: #29C16E; width:60px; height:28px" ></b-icon>
+              97%
+            </b-col>
+            <b-col>
+              <b-card bg-variant="secondary" text-variant="white" style="width:165px; height:25px;" no-body>Mode: Autonomous</b-card>
+            </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-button @click="show-true" v-b-modal.modal-1 variant="primary" class="mb-2" style="width:230px; height:50px">
+              <b-icon icon="exclamation-circle-fill" variant="white"></b-icon> STAGE SELECTION
+            </b-button>
+
+            <b-modal id="modal-1" title="Stage Selection">
+              <p class="my-4">This is Stage Selection</p>
+                <template #modal-footer="{ok, cancel}">
+                 <!-- Emulate built in modal footer ok and cancel button actions -->
+                <b-button size="sm" variant="success" @click="ok()">Yes</b-button>
+                <b-button size="sm" variant="danger" @click="cancel()">No</b-button>
+                </template>
+            </b-modal>
+          </b-col>
+          <b-col>
+            <b-button @click="show-true" v-b-modal.modal-2 variant="danger" class="mb-2" style="width:230px; height:50px">
+              
+              <b-icon icon="exclamation-circle-fill" variant="white"></b-icon> EMERGENCY STOP
+            </b-button>
+
+            <b-modal id="modal-2" title="Emergency Stop">
+              <p class="my-4">Are you sure?</p>
+              <template #modal-footer="{ ok, cancel }">
+                 <!-- Emulate built in modal footer ok and cancel button actions -->
+                <b-button size="sm" variant="success" @click="ok()">Yes</b-button>
+                <b-button size="sm" variant="danger" @click="cancel()">No</b-button>
+              </template>
+            </b-modal>
+          </b-col>
+        </b-row>
+      </b-card>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {};
+</script>
+
+<style>
+img {
+  display: block;
+  width: 20;
+  height: 20;
+}
+</style>
--- front-end/src/views/Main.vue
+++ front-end/src/views/Main.vue
@@ -1,38 +1,40 @@
 <template>
-  <div id="app">
-        <b-row>
-          <br />
-          <br />
-        </b-row>
-        <b-row class="noPadding">
-                <br /> 
-                <b-col align="left">
-                  <!-- RESIZE IMG -->
-                  <div id="image">
-                    <b-img :src="require('../assets/map.png')" class="img-fluid" rounded alt='map pic'></b-img>
-                  </div>
-                </b-col> 
-                <br />     
-                <!-- ALIGN COLUMNS RIGHT A LITTLE BIT MORE AND MAKE WIDER -->
-                <b-col cols="5">
-                      <b-card title='ERU'>
-                        <h1>ERU</h1>
-                      </b-card>  
-                      &nbsp;
-                      <b-card title='MAC'>
-                        <h1>MAC</h1>
-                      </b-card> 
-                      &nbsp;
-                      <b-card title='MEA'>
-                        <h1>MEA</h1>
-                      </b-card> 
-                </b-col>
-        </b-row>
+  <div>
+    <b-row class="noPadding">
+      <br />
+      <b-col align="left">
+        <!-- RESIZE IMG -->
+        <div id="image">
+          <b-img
+            :src="require('../assets/map.png')"
+            class="img-fluid"
+            rounded
+            alt="map pic"
+          ></b-img>
+        </div>
+      </b-col>
+      <br />
+      <!-- ALIGN COLUMNS RIGHT A LITTLE BIT MORE AND MAKE WIDER -->
+      <b-col cols="5">
+        <VehicleStatus />
+        &nbsp;
+        <VehicleStatus />
+        &nbsp;
+        <VehicleStatus />
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-export default {}
+// @ is the same as src
+import VehicleStatus from "@/components/Main/VehicleStatus.vue";
+
+export default {
+  components: {
+    VehicleStatus,
+  },
+};
 </script>
 
 <style></style>
