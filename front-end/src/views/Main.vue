3dd037ef715e74ed22ecf543590b3972d0acefc8
matthewkhle <mattmattmattle@gmail.com>
2021-11-05 11:31:50 -0700

    Update Main.vue

--- front-end/src/views/Main.vue
+++ front-end/src/views/Main.vue
@@ -1,6 +1,34 @@
 <template>
-	<!-- <h1>Main</h1> -->
-    <b-img width="750px" :src="require('../assets/map.png')"></b-img>
+  <div id="app">
+        <b-row>
+          <br />
+          <br />
+        </b-row>
+        <b-row class="noPadding">
+                <br /> 
+                <b-col align="left">
+                  <!-- RESIZE IMG -->
+                  <div id="image">
+                    <b-img :src="require('../assets/map.png')" class="img-fluid" rounded alt='map pic'></b-img>
+                  </div>
+                </b-col> 
+                <br />     
+                <!-- ALIGN COLUMNS RIGHT A LITTLE BIT MORE AND MAKE WIDER -->
+                <b-col cols="5">
+                      <b-card title='ERU'>
+                        <h1>ERU</h1>
+                      </b-card>  
+                      &nbsp;
+                      <b-card title='MAC'>
+                        <h1>MAC</h1>
+                      </b-card> 
+                      &nbsp;
+                      <b-card title='MEA'>
+                        <h1>MEA</h1>
+                      </b-card> 
+                </b-col>
+        </b-row>
+  </div>
 </template>
 
 <script>
