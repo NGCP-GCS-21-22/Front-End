e3089d18b33df696ad39e533577a7a3919d0e43c
matthewkhle <mattmattmattle@gmail.com>
2021-11-12 09:19:35 -0800

    Updated Form.vue
    - still need further updates pushed

--- front-end/src/views/Start/Form.vue
+++ front-end/src/views/Start/Form.vue
@@ -1,20 +1,33 @@
 <template>
-    <div>
-        <h1>the form!!!</h1>
-        <b-button @click="goToGCS"> go to GCS</b-button>
-    </div>
+  <div>
+    <h1>hello there</h1>
+    <b-button @click="goToGCS"> go to GCS</b-button>
+  </div>
 </template>
 
 <script>
 export default {
-    methods: {
-        goToGCS() {
-            this.$router.push("/gcs/main")
-        }
-    }
-}
+  data() {
+    return {
+      longitude: null,
+      latitude: null,
+    };
+  },
+  methods: {
+    goToGCS() {
+      this.$router.push("/gcs/main");
+    },
+  },
+};
 </script>
 
 <style>
-
+.btn-circle.btn-sm {
+  width: 30px;
+  height: 30px;
+  padding: 6px 0px;
+  border-radius: 15px;
+  font-size: 8px;
+  text-align: center;
+}
 </style>
