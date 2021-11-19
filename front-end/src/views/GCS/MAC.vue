4714c7d8ec7a11c6447028be29f5ed8311b60f8d
matthewkhle <47379008+matthewkhle@users.noreply.github.com>
2021-11-19 15:17:18 -0800

    Update MAC.vue

--- front-end/src/views/GCS/MAC.vue
+++ front-end/src/views/GCS/MAC.vue
@@ -1,46 +1,46 @@
 <template>
-  <div>
-    <b-row class="noPadding">
-      <br />
-      <!-- left column -->
-      <b-col align="left">
-        <!-- RESIZE IMG -->
-        <div id="image">
-          <b-img
-            :src="require('@/assets/map.png')"
-            class="img-fluid"
-            rounded
-            alt="map pic"
-          ></b-img>
-        </div>
-      </b-col>
-      <br />
-      <!-- ALIGN COLUMNS RIGHT A LITTLE BIT MORE AND MAKE WIDER -->
-      <!-- right column -->
-      <b-col cols="5">
-        <b-row>
-        <!-- A Card with dummy data for Status MAC -->
-        <Status/>
-        </b-row>
-        <!-- &nbsp; -->
-        <b-row>
-        <!-- A Card with 6 buttons inside -->
-        <Widgets/>
-        </b-row>
-      </b-col>
-    </b-row>
-  </div>
+	<div>
+		<b-row class="noPadding">
+			<br />
+			<!-- left column -->
+			<b-col align="left">
+				<!-- RESIZE IMG -->
+				<div id="image">
+					<b-img
+						:src="require('@/assets/map.png')"
+						class="img-fluid"
+						rounded
+						alt="map pic"
+					></b-img>
+				</div>
+			</b-col>
+			<br />
+			<!-- ALIGN COLUMNS RIGHT A LITTLE BIT MORE AND MAKE WIDER -->
+			<!-- right column -->
+			<b-col cols="5">
+				<b-row>
+					<!-- A Card with dummy data for Status MAC -->
+					<Status />
+				</b-row>
+				<!-- &nbsp; -->
+				<b-row>
+					<!-- A Card with 6 buttons inside -->
+					<Widgets />
+				</b-row>
+			</b-col>
+		</b-row>
+	</div>
 </template>
 
 <script>
 import Status from "@/components/VehiclePage/Status.vue";
 import Widgets from "@/components/VehiclePage/Widgets.vue";
 
-
 export default {
-  components: {
-    Status, Widgets
-  },
+	components: {
+		Status,
+		Widgets,
+	},
 };
 </script>
 
