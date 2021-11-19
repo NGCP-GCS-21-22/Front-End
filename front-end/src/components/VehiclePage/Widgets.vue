11cae39a5328581f1fb0b9baf4b2f0e1b9eebb3f
matthewkhle <47379008+matthewkhle@users.noreply.github.com>
2021-11-19 15:52:00 -0800

    Fix toggling
    Co-Authored-By: vaibhavi8 <31997056+vaibhavi8@users.noreply.github.com>
    Co-Authored-By: Nhi Nguyen <conniewongntn@gmail.com>

---
+++ front-end/src/components/VehiclePage/Widgets.vue
@@ -0,0 +1,120 @@
+<template>
+	<b-container fluid="sm">
+		<b-card>
+			<div v-for="widget in widgetList" :key="widget.name">
+				<b-container>
+					<b-row>
+						<b-button
+							v-if="!selected"
+							@click="selectWidget(widget.name)"
+						>
+							{{ widget.name }}
+						</b-button>
+
+						<Waypoint
+							v-if="
+								widget.type == 'Waypoint' &&
+								cardSelected == widget.name
+							"
+							:name="widget.name"
+							@goBack="showWidgets"
+						/>
+						<StageCommand
+							v-if="
+								widget.type == 'StageCommand' &&
+								cardSelected == widget.name
+							"
+							:name="widget.name"
+							@goBack="showWidgets"
+						/>
+						<SearchArea
+							v-if="
+								widget.type == 'SearchArea' &&
+								cardSelected == widget.name
+							"
+							:name="widget.name"
+						/>
+						<Geofence
+							v-if="
+								widget.type == 'Geofence' &&
+								cardSelected == widget.name
+							"
+							:name="widget.name"
+						/>
+						<ManualControl
+							v-if="
+								widget.type == 'ManualControl' &&
+								cardSelected == widget.name
+							"
+							:name="widget.name"
+						/>
+					</b-row>
+				</b-container>
+			</div>
+		</b-card>
+	</b-container>
+</template>
+
+<script>
+import Waypoint from "@/components/Widgets/Waypoint.vue";
+import StageCommand from "@/components/Widgets/StageCommand.vue";
+import SearchArea from "@/components/Widgets/SearchArea.vue";
+import Geofence from "@/components/Widgets/Geofence.vue";
+import ManualControl from "@/components/Widgets/ManualControl.vue";
+
+export default {
+	components: {
+		Waypoint,
+		StageCommand,
+		SearchArea,
+		Geofence,
+		ManualControl,
+	},
+	data() {
+		return {
+			selected: false,
+			cardSelected: null,
+
+			widgetList: [
+				{
+					type: "Waypoint",
+					name: "ERU Drop Location",
+				},
+				{
+					type: "Waypoint",
+					name: "Home Coordinates",
+				},
+				{
+					type: "StageCommand",
+					name: "Stage Command",
+				},
+				{
+					type: "SearchArea",
+					name: "Search Area",
+				},
+				{
+					type: "Geofence",
+					name: "Geofence",
+				},
+				{
+					type: "ManualControl",
+					name: "Manual Control",
+				},
+			],
+		};
+	},
+	methods: {
+		selectWidget(widgetName) {
+			this.selected = true;
+			this.cardSelected = widgetName;
+		},
+		showWidgets() {
+			this.selected = false;
+			this.cardSelected = null;
+		},
+	},
+};
+</script>
+
+<style>
+</style>
