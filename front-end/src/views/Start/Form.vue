1017cfc7d28f65e93d9c8284e7afcdb3fc6622c5
matthewkhle <47379008+matthewkhle@users.noreply.github.com>
2022-02-20 13:49:07 -0800

    Update Form.vue
    - modifying comments and spacing

--- front-end/src/views/Start/Form.vue
+++ front-end/src/views/Start/Form.vue
@@ -1,31 +1,35 @@
 <template>
 	<div>
-			<h1 style="color: white;">Form Page</h1>
-			<b-button class="back" variant="outline-primary" @click="goToStart">Back</b-button>
-			<b-button class="back" variant="outline-primary" @click="goToGCS">Go to GCS</b-button>
+		<h1 style="color: white">Form Page</h1>
+		<b-button class="back" variant="outline-primary" @click="goToStart"
+			>Back</b-button
+		>
+		<b-button class="back" variant="outline-primary" @click="goToGCS"
+			>Go to GCS</b-button
+		>
 
-			<b-container fluid>
-				<b-row class="justify-content-md-center">
-					<b-col md="4"> 
-						<MissionForm ref="MACForm" :vehicleName="'MAC'" />
-					</b-col>
-					<b-col cols="4" > 
-						<MissionForm ref="ERUForm" :vehicleName="'ERU'" />
-					</b-col>
-					<b-col cols="4"> 
-						<MissionForm ref="MEAForm" :vehicleName="'MEA'" />
-					</b-col>
-				</b-row>
-			</b-container>
-			<b-button class="submit" variant="primary" @click.prevent="submit()">Submit</b-button>
+		<b-container fluid>
+			<b-row class="justify-content-md-center">
+				<b-col md="4">
+					<MissionForm ref="MACForm" :vehicleName="'MAC'" />
+				</b-col>
+				<b-col cols="4">
+					<MissionForm ref="ERUForm" :vehicleName="'ERU'" />
+				</b-col>
+				<b-col cols="4">
+					<MissionForm ref="MEAForm" :vehicleName="'MEA'" />
+				</b-col>
+			</b-row>
+		</b-container>
+		<b-button class="submit" variant="primary" @click.prevent="submit()"
+			>Submit</b-button
+		>
 	</div>
 </template>
 
 <script>
 import MissionForm from "@/components/Form/MissionForm.vue";
-import axios from 'axios'
-
-
+import axios from "axios";
 
 export default {
 	components: {
@@ -34,8 +38,7 @@ export default {
 	props: {
 		vehicleName: String,
 	},
-	
-	
+
 	methods: {
 		goToGCS() {
 			this.$router.push("/gcs/main");
@@ -44,61 +47,67 @@ export default {
 			this.$router.push("/");
 		},
 		submit() {
-			// get the waypoint from each mission form
-			console.log(this.$refs.MACForm.$refs.MissionWaypoint.waypointName);
-			console.log(this.$refs.ERUForm.$refs.MissionWaypoint.waypointName);
-			console.log(this.$refs.MEAForm.$refs.MissionWaypoint.waypointName);
-			// get the Stage id and name from each mission form
-			console.log(this.$refs.MACForm.$refs.Stages.id);
-			console.log(this.$refs.ERUForm.$refs.Stages.id);
-			console.log(this.$refs.MEAForm.$refs.Stages.id);
-			console.log(this.$refs.MACForm.$refs.Stages.stage);
-			console.log(this.$refs.ERUForm.$refs.Stages.stage);
-			console.log(this.$refs.MEAForm.$refs.Stages.stage);
-			// get the manual control from each mission form
-			console.log(this.$refs.MACForm.$refs.ManualControl.toggle);
-			console.log(this.$refs.ERUForm.$refs.ManualControl.toggle);
-			console.log(this.$refs.MEAForm.$refs.ManualControl.toggle);
-			// get the search area from each mission form
-			console.log(this.$refs.MACForm.$refs.SearchArea.toggle); 
-			console.log(this.$refs.ERUForm.$refs.SearchArea.toggle); 
-			console.log(this.$refs.MEAForm.$refs.SearchArea.toggle); 
+			// // get the waypoint from each mission form
+			// console.log(this.$refs.MACForm.$refs.MissionWaypoint.waypointName);
+			// console.log(this.$refs.ERUForm.$refs.MissionWaypoint.waypointName);
+			// console.log(this.$refs.MEAForm.$refs.MissionWaypoint.waypointName);
+			// // get the Stage id and name from each mission form
+			// console.log(this.$refs.MACForm.$refs.Stages.id);
+			// console.log(this.$refs.ERUForm.$refs.Stages.id);
+			// console.log(this.$refs.MEAForm.$refs.Stages.id);
+			// console.log(this.$refs.MACForm.$refs.Stages.stage);
+			// console.log(this.$refs.ERUForm.$refs.Stages.stage);
+			// console.log(this.$refs.MEAForm.$refs.Stages.stage);
+			// // get the manual control from each mission form
+			// console.log(this.$refs.MACForm.$refs.ManualControl.toggle);
+			// console.log(this.$refs.ERUForm.$refs.ManualControl.toggle);
+			// console.log(this.$refs.MEAForm.$refs.ManualControl.toggle);
+			// // get the search area from each mission form
+			// console.log(this.$refs.MACForm.$refs.SearchArea.toggle);
+			// console.log(this.$refs.ERUForm.$refs.SearchArea.toggle);
+			// console.log(this.$refs.MEAForm.$refs.SearchArea.toggle);
 
-			//const submit = (vehicleName) => {
-				let path = "http://localhost:5000/postData"
-				let payload = {
-					MAC:{
-						icon: "https://github.com/NGCP-GCS-21-22/Front-End/blob/main/front-end/src/assets/MAC.png",
-						MissionWaypoint: this.$refs.MACForm.$refs.MissionWaypoint.waypointName,
-						Stage: this.$refs.MACForm.$refs.Stages.id,
-						Stage: this.$refs.MACForm.$refs.Stages.stage,
-						Manualcontrol: this.$refs.MACForm.$refs.ManualControl.toggle,
-						Searcharea: this.$refs.MACForm.$refs.SearchArea.toggle
-					},
-					ERU:{
-						icon: "https://github.com/NGCP-GCS-21-22/Front-End/blob/main/front-end/src/assets/ERU.png",
-						MissionWaypoint: this.$refs.ERUForm.$refs.MissionWaypoint.waypointName,
-						Stage: this.$refs.ERUForm.$refs.Stages.id,
-						Stage: this.$refs.ERUForm.$refs.Stages.stage,
-						Manualcontrol: this.$refs.ERUForm.$refs.ManualControl.toggle,
-						Searcharea: this.$refs.ERUForm.$refs.SearchArea.toggle
-					},
-					MEA:{
-						icon: "https://github.com/NGCP-GCS-21-22/Front-End/blob/main/front-end/src/assets/MEA.png",
-						MissionWaypoint: this.$refs.MEAForm.$refs.MissionWaypoint.waypointName,
-						Stage: this.$refs.MEAForm.$refs.Stages.id,
-						Stage: this.$refs.MEAForm.$refs.Stages.stage,
-						Manualcontrol: this.$refs.MEAForm.$refs.ManualControl.toggle,
-						Searcharea: this.$refs.MEAForm.$refs.SearchArea.toggle
-					},
-				}
-				axios.post(path, payload)
-				.then((response) => {
+			let path = "http://localhost:5000/postData";
+			let payload = {
+				MAC: {
+					icon: "https://github.com/NGCP-GCS-21-22/Front-End/blob/main/front-end/src/assets/MAC.png",
+					MissionWaypoint:
+						this.$refs.MACForm.$refs.MissionWaypoint.waypointName,
+					Stage: this.$refs.MACForm.$refs.Stages.id,
+					Stage: this.$refs.MACForm.$refs.Stages.stage,
+					Manualcontrol:
+						this.$refs.MACForm.$refs.ManualControl.toggle,
+					Searcharea: this.$refs.MACForm.$refs.SearchArea.toggle,
+				},
+				ERU: {
+					icon: "https://github.com/NGCP-GCS-21-22/Front-End/blob/main/front-end/src/assets/ERU.png",
+					MissionWaypoint:
+						this.$refs.ERUForm.$refs.MissionWaypoint.waypointName,
+					Stage: this.$refs.ERUForm.$refs.Stages.id,
+					Stage: this.$refs.ERUForm.$refs.Stages.stage,
+					Manualcontrol:
+						this.$refs.ERUForm.$refs.ManualControl.toggle,
+					Searcharea: this.$refs.ERUForm.$refs.SearchArea.toggle,
+				},
+				MEA: {
+					icon: "https://github.com/NGCP-GCS-21-22/Front-End/blob/main/front-end/src/assets/MEA.png",
+					MissionWaypoint:
+						this.$refs.MEAForm.$refs.MissionWaypoint.waypointName,
+					Stage: this.$refs.MEAForm.$refs.Stages.id,
+					Stage: this.$refs.MEAForm.$refs.Stages.stage,
+					Manualcontrol:
+						this.$refs.MEAForm.$refs.ManualControl.toggle,
+					Searcharea: this.$refs.MEAForm.$refs.SearchArea.toggle,
+				},
+			};
+			axios.post(path, payload).then(
+				(response) => {
 					console.log("Mission data sent.");
-				}, (error) => {
+				},
+				(error) => {
 					console.log(Error);
-				});
-			//}
+				}
+			);
 		},
 	},
 };
@@ -115,16 +124,15 @@ export default {
 	margin-bottom: 10px;
 }
 
-.submit{
-	 text-align: center;
-	 margin-top: 20px;
+.submit {
+	text-align: center;
+	margin-top: 20px;
 }
 
-.back{
+.back {
 	margin-top: 20px;
 	margin-bottom: 20px;
 	margin-left: 10px;
 	margin-right: 10px;
 }
-
 </style>
