f653560b9c6e7b7afe0a1fdd84bd890cf4ec0308
Matthew Le <le2matthew@gmail.com>
2022-03-17 16:53:35 -0700

    Update PrincipalAxes.vue

--- front-end/src/components/VehiclePage/StatusComponents/PrincipalAxes.vue
+++ front-end/src/components/VehiclePage/StatusComponents/PrincipalAxes.vue
@@ -1,36 +1,52 @@
 <template>
-	<b-row>
-		<b-col class="info"><u>Pitch</u><br /> {{ pitch }}</b-col>
-		<b-col class="info"><u>Roll</u><br /> {{ roll }} </b-col>
-		<b-col class="info"><u>Yaw</u><br /> {{ yaw }} </b-col>
-	</b-row>
+    <b-container>
+        <b-row class="principal-axes-row">
+            <b-col class="info"
+                ><u>Pitch</u><br />
+                {{ pitch }}</b-col
+            >
+            <b-col class="info"
+                ><u>Roll</u><br />
+                {{ roll }}
+            </b-col>
+            <b-col class="info"
+                ><u>Yaw</u><br />
+                {{ yaw }}
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
 export default {
-	props: {
-		vehicleData: Object,
-	},
-	computed: {
-		pitch() {
-			if (!this.vehicleData) return 0;
-			return this.vehicleData["pitch"];
-		},
-		roll() {
-			if (!this.vehicleData) return 0;
-			return this.vehicleData["roll"];
-		},
-		yaw() {
-			if (!this.vehicleData) return 0;
-			return this.vehicleData["yaw"];
-		},
-	},
+    props: {
+        vehicleData: Object,
+    },
+    computed: {
+        pitch() {
+            if (!this.vehicleData) return 0;
+            return this.vehicleData["pitch"];
+        },
+        roll() {
+            if (!this.vehicleData) return 0;
+            return this.vehicleData["roll"];
+        },
+        yaw() {
+            if (!this.vehicleData) return 0;
+            return this.vehicleData["yaw"];
+        },
+    },
 };
 </script>
 
 <style>
 .info {
-	font-size: 20pt;
-	padding-bottom: 5px;
+    font-size: 20pt;
+	padding: 1vh;
+    /* padding-bottom: 5vh; */
+}
+
+.principal-axes-row {
+    height: 13vh;
 }
 </style>
