123c0f3e301afa01b05bc17d2e09bcdf6c3f3df0
Matthew Le <le2matthew@gmail.com>
2022-03-18 13:48:12 -0700

    Bring back changes to search area and geofence polygons
    Co-Authored-By: April Trinh <56134712+soupbara@users.noreply.github.com>

--- front-end/src/components/Maps/VehicleMap.vue
+++ front-end/src/components/Maps/VehicleMap.vue
@@ -73,7 +73,7 @@
             <GmapPolygon
                 :path="searchAreaPolygon.coordinates"
                 :clickable="false"
-                :options="polyOptions"
+                :options="searchAreaOptions"
             />
 		<!-- Geofence -->
         <!-- Workspace -->
@@ -322,110 +322,128 @@ export default {
         },
     },
     data() {
-        return {
-            // Map Data
+		return {
+			// Map Data
             hover: false,
-            center: { lat: centerLat, lng: centerLng },
-            zoom: 18,
-            tilt: 0,
-            options: {
-                zoomControl: false,
-                mapTypeControl: false,
-                scaleControl: false,
-                streetViewControl: false,
-                rotateControl: false,
-                fullscreenControl: false,
-                scrollwheel: false,
-                draggable: false,
-                disableDoubleClickZoom: true,
-            },
-            mapType: "satellite",
-
-            // Polygon Data
-            // paths: [
-            // 	{ id: 1, lat: 33.933729, lng: -117.6318437 }, // marker1
-            // 	{ id: 2, lat: 33.93441, lng: -117.6318169 }, // marker2
-            // 	{ id: 3, lat: 33.9344055, lng: -117.6306099 },
-            // 	{ id: 4, lat: 33.9337468, lng: -117.6305616 },
-            // ],
-            paths: [],
-
-            polyOptions: {
-                strokeColor: "#39FF14",
-                strokeOpacity: 0.8,
-                strokeWeight: 3,
-                fillColor: "#39FF14",
-                fillOpacity: 0.3,
-            },
-        };
-    },
-    mounted() {},
-    methods: {
-        isSelected(marker) {
-            if (!marker || !marker.draggable) return false;
-            return true;
-        },
-        moveMissionWaypoint(e) {
-            this.missionWaypointMarker.position = {
-                lat: e.latLng.lat(),
-                lng: e.latLng.lng(),
-            };
-            this.$emit("moveMarker", "missionWaypoint", {
-                lat: this.missionWaypointMarker.position.lat,
-                lng: this.missionWaypointMarker.position.lng,
-            });
-        },
-        moveHomeCoordinates(e) {
-            this.homeCoordinatesMarker.position = {
-                lat: e.latLng.lat(),
-                lng: e.latLng.lng(),
-            };
-            this.$emit("moveMarker", "homeCoordinates", {
-                lat: this.homeCoordinatesMarker.position.lat,
-                lng: this.homeCoordinatesMarker.position.lng,
-            });
-        },
-        moveSearchAreaVertex(e, index) {
-            this.searchAreaPolygon.coordinates[index].lat = e.latLng.lat();
-            this.searchAreaPolygon.coordinates[index].lng = e.latLng.lng();
-
-            this.$emit(
-                "moveMarker",
-                "searchArea",
-                this.searchAreaPolygon.coordinates
-            );
-        },
-        getWorkspacePolyOption() {
-            if (this.geofenceWorkspace.keep_in == null) return null;
-            if (this.geofenceWorkspace.keep_in == true) return keepInOptions;
-            if (this.geofenceWorkspace.keep_in == false) return keepOutOptions;
-        },
-        moveGeofenceWorkspaceVertex(e, index) {
-            this.geofenceWorkspacePolygon.coordinates[index].lat =
-                e.latLng.lat();
-            this.geofenceWorkspacePolygon.coordinates[index].lng =
-                e.latLng.lng();
-            this.$emit("moveMarker", "geofenceWorkspace", {
-                coordinates: this.geofenceWorkspacePolygon.coordinates,
-                keep_in: this.geofenceWorkspacePolygon.keepIn,
-            });
-        },
-        getCenter(coordinates) {
-            let centerLat = 0;
-            let centerLng = 0;
-            for (let i = 0; i < coordinates.length; i++) {
-                let coordinate = coordinates[i];
-                centerLat += parseFloat(coordinate.lat);
-                centerLng += parseFloat(coordinate.lng);
-            }
-            centerLat = centerLat / coordinates.length;
-            centerLng = centerLng / coordinates.length;
-            return {
-                lat: parseFloat(centerLat),
-                lng: parseFloat(centerLng),
-            };
-        },
-    },
+			center: { lat: centerLat, lng: centerLng },
+			zoom: 18,
+			tilt: 0,
+			options: {
+				zoomControl: false,
+				mapTypeControl: false,
+				scaleControl: false,
+				streetViewControl: false,
+				rotateControl: false,
+				fullscreenControl: false,
+				scrollwheel: false,
+				draggable: false,
+				disableDoubleClickZoom: true,
+			},
+			mapType: "satellite",
+			// Polygon Data
+			// paths: [
+			// 	{ id: 1, lat: 33.933729, lng: -117.6318437 }, // marker1
+			// 	{ id: 2, lat: 33.93441, lng: -117.6318169 }, // marker2
+			// 	{ id: 3, lat: 33.9344055, lng: -117.6306099 },
+			// 	{ id: 4, lat: 33.9337468, lng: -117.6305616 },
+			// ],
+			paths: [],
+			polyOptions: {
+				strokeColor: "#39FF14",
+				strokeOpacity: 0.8,
+				strokeWeight: 3,
+				fillColor: "#39FF14",
+				fillOpacity: 0.3,
+			},
+			searchAreaOptions: {
+				strokeColor: "#FFA500",
+				strokeOpacity: 0.8,
+				strokeWeight: 3,
+				fillColor: "#FFA500",
+				fillOpacity: 0.3,
+			},
+			keepInOptions: {
+				strokeColor: "#39FF14",
+				strokeOpacity: 0.8,
+				strokeWeight: 3,
+				fillColor: "#39FF14",
+				fillOpacity: 0.3,
+			},
+			keepOutOptions: {
+				strokeColor: "#cc0000",
+				strokeOpacity: 0.8,
+				strokeWeight: 3,
+				fillColor: "#cc0000",
+				fillOpacity: 0.3,
+			},
+		}
+	},
+	mounted() {},
+	methods: {
+		isSelected(marker) {
+			if (!marker || !marker.draggable) return false
+			return true
+		},
+		moveMissionWaypoint(e) {
+			this.missionWaypointMarker.position = {
+				lat: e.latLng.lat(),
+				lng: e.latLng.lng(),
+			}
+			this.$emit("moveMarker", "missionWaypoint", {
+				lat: this.missionWaypointMarker.position.lat,
+				lng: this.missionWaypointMarker.position.lng,
+			})
+		},
+		moveHomeCoordinates(e) {
+			this.homeCoordinatesMarker.position = {
+				lat: e.latLng.lat(),
+				lng: e.latLng.lng(),
+			}
+			this.$emit("moveMarker", "homeCoordinates", {
+				lat: this.homeCoordinatesMarker.position.lat,
+				lng: this.homeCoordinatesMarker.position.lng,
+			})
+		},
+		moveSearchAreaVertex(e, index) {
+			this.searchAreaPolygon.coordinates[index].lat = e.latLng.lat()
+			this.searchAreaPolygon.coordinates[index].lng = e.latLng.lng()
+			this.$emit(
+				"moveMarker",
+				"searchArea",
+				this.searchAreaPolygon.coordinates
+			)
+		},
+		getWorkspacePolyOption() {
+			if (this.geofenceWorkspace.keep_in == null) return null
+			if (this.geofenceWorkspace.keep_in == true) return keepInOptions
+			if (this.geofenceWorkspace.keep_in == false) return keepOutOptions
+		},
+		moveGeofenceWorkspaceVertex(e, index) {
+			this.geofenceWorkspacePolygon.coordinates[index].lat =
+				e.latLng.lat()
+			this.geofenceWorkspacePolygon.coordinates[index].lng =
+				e.latLng.lng()
+			this.$emit("moveMarker", "geofenceWorkspace", {
+				coordinates: this.geofenceWorkspacePolygon.coordinates,
+				keep_in: this.geofenceWorkspacePolygon.keepIn,
+			})
+		},
+		getCenter(coordinates) {
+			let centerLat = 0
+			let centerLng = 0
+			for (let i = 0; i < coordinates.length; i++) {
+				let coordinate = coordinates[i]
+				centerLat += parseFloat(coordinate.lat)
+				centerLng += parseFloat(coordinate.lng)
+			}
+			centerLat = centerLat/coordinates.length
+			centerLng = centerLng/coordinates.length
+			return {
+				lat: parseFloat(centerLat),
+				lng: parseFloat(centerLng),
+			}
+		}
+	},
 };
 </script>
 
