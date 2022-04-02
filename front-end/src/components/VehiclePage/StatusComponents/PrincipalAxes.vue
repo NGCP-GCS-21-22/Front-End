1976ad70049540e2bdf874562b0180cc2025ee83
matthewkhle <47379008+matthewkhle@users.noreply.github.com>
2022-04-02 11:27:56 -0700

    #88 Add colors to pitch roll and yaw
    Co-Authored-By: April Trinh <56134712+soupbara@users.noreply.github.com>

--- front-end/src/components/VehiclePage/StatusComponents/PrincipalAxes.vue
+++ front-end/src/components/VehiclePage/StatusComponents/PrincipalAxes.vue
@@ -1,52 +1,64 @@
 <template>
-    <b-container>
-        <b-row class="principal-axes-row">
-            <b-col class="info"
-                ><u>Pitch</u><br />
-                {{ pitch }}</b-col
-            >
-            <b-col class="info"
-                ><u>Roll</u><br />
-                {{ roll }}
-            </b-col>
-            <b-col class="info"
-                ><u>Yaw</u><br />
-                {{ yaw }}
-            </b-col>
-        </b-row>
-    </b-container>
+	<b-container>
+		<b-row class="principal-axes-row">
+			<b-col class="info">
+				<u>Pitch</u><br />
+				<span :style="pitchStyle">{{ pitch }}</span>
+			</b-col>
+			<b-col class="info">
+				<u>Roll</u><br />
+				<span :style="rollStyle">{{ roll }}</span>
+			</b-col>
+			<b-col class="info">
+				<u>Yaw</u><br />
+				<span :style="yawStyle">{{ yaw }}</span>
+			</b-col>
+		</b-row>
+	</b-container>
 </template>
 
 <script>
 export default {
-    props: {
-        vehicleData: Object,
-    },
-    computed: {
-        pitch() {
-            if (!this.vehicleData) return 0;
-            return this.vehicleData["pitch"];
-        },
-        roll() {
-            if (!this.vehicleData) return 0;
-            return this.vehicleData["roll"];
-        },
-        yaw() {
-            if (!this.vehicleData) return 0;
-            return this.vehicleData["yaw"];
-        },
-    },
+	props: {
+		vehicleData: Object,
+	},
+	computed: {
+		pitch() {
+			if (!this.vehicleData) return 0;
+			return this.vehicleData["pitch"];
+		},
+		roll() {
+			if (!this.vehicleData) return 0;
+			return this.vehicleData["roll"];
+		},
+		yaw() {
+			if (!this.vehicleData) return 0;
+			return this.vehicleData["yaw"];
+		},
+		pitchStyle() {
+			if (!this.vehicleData) return null;
+			return `color: ${this.vehicleData["pitch_color"]}`;
+		},
+		rollStyle() {
+			if (!this.vehicleData) return nul;
+			return `color: ${this.vehicleData["roll_color"]}`;
+		},
+		yawStyle() {
+			if (!this.vehicleData) return nul;
+			return `color: ${this.vehicleData["yaw_color"]}`;
+		},
+	},
 };
 </script>
 
-<style>
+<style scoped>
 .info {
-    font-size: 20pt;
+	font-size: 20pt;
 	padding: 1vh;
-    /* padding-bottom: 5vh; */
+	/* padding-bottom: 5vh; */
 }
 
 .principal-axes-row {
-    height: 13vh;
+	height: 13vh;
 }
 </style>
