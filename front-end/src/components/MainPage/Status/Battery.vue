5b00e1436b1acaf985423186768497153d1616e5
Matthew Le <le2matthew@gmail.com>
2022-05-07 12:45:31 -0700

    Battery now changes appereance
    Co-Authored-By: April Trinh <56134712+soupbara@users.noreply.github.com>

--- front-end/src/components/MainPage/Status/Battery.vue
+++ front-end/src/components/MainPage/Status/Battery.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="batteryContainer">
-		<!-- <b-icon
+    <div class="batteryContainer">
+        <!-- <b-icon
 			v-if="batteryPct >= 80"
 			icon="battery-full"
 			class="batteryFull"
@@ -15,57 +15,53 @@
 			icon="battery"
 			class="batteryLow"
 		></b-icon> -->
-		<b-icon class="battery" :style="batteryStyle" :icon="batteryIcon">
-		</b-icon>
-		<div class="batteryPercent">{{ batteryPct }}%</div>
-	</div>
+        <b-icon class="battery" :style="batteryStyle" :icon="batteryIcon">
+        </b-icon>
+        <div class="batteryPercent">{{ batteryPct }}%</div>
+    </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {};
-	},
-	props: {
-		batteryPct: Number,
-		batteryColor: String,
-	},
-	computed: {
-		batteryIcon() {
-			// MODIFY THE DEFAULT POSSIBLY
-			if (!this.batteryColor) return "battery";
+    data() {
+        return {};
+    },
+    props: {
+        batteryPct: Number,
+        batteryColor: String,
+    },
+    computed: {
+        batteryIcon() {
+            if (!this.batteryPct) return "battery";
 
-			if (this.batteryColor == "Green") {
-				return "battery-full";
-			} else if (this.batteryColor == "Yellow") {
-				return "battery-half";
-			} else if (this.batteryColor == "Red") {
-				return "battery";
-			}
-		},
-		batteryStyle() {
-			if (!this.batteryColor) return null;
-			return `color: ${this.batteryColor}`;
-		},
-	},
+            if (this.batteryPct < 33) return "battery";
+            else if (this.batteryPct < 66) return "battery-half";
+			else return "battery-full";
+        },
+        batteryStyle() {
+            if (this.batteryPct < 33) return `color: red`;
+            else if (this.batteryPct < 66) return `color: yellow`;
+            else return `color: green`;
+        },
+    },
 };
 </script>
 
 <style scoped>
 .batteryContainer {
-	overflow: auto;
-	padding: 10px 0px;
+    overflow: auto;
+    padding: 10px 0px;
 }
 
 .battery {
-	width: 60px;
-	height: 38px;
-	margin-top: -8px;
-	float: left;
+    width: 60px;
+    height: 38px;
+    margin-top: -8px;
+    float: left;
 }
 
 .batteryPercent {
-	float: left;
-	font-size: 11pt;
+    float: left;
+    font-size: 11pt;
 }
 </style>
