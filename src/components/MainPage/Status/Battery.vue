0a75f3e3f3dc0e5c7b665fe376067c4ce6944302
iPhelps710200 <iphelps@cpp.edu>
2022-10-19 09:20:20 -0700

    Update Battery.vue

--- src/components/MainPage/Status/Battery.vue
+++ src/components/MainPage/Status/Battery.vue
@@ -3,7 +3,7 @@
         <b-icon class="battery" :style="batteryStyle" :icon="batteryIcon">
         </b-icon>
         <div v-if="batteryPct == -1" class="batteryPercent">--%</div>
-        <div v-else class="batteryPercent">{{ batteryPct }}%</div>
+        <div v-else class="batteryPercent">{{ batteryPct }}%</div> 
     </div>
 </template>
 
@@ -13,21 +13,21 @@ export default {
         return {};
     },
     props: {
-        batteryPct: Number,
+        batteryPct: Number,                                               // declare a property called batteryPct that is a number 
     },
     computed: {
         batteryIcon() {
-            if (!this.batteryPct) return "battery";
+            if (!this.batteryPct) return "battery";                       // check if battery dead return string warning "battery" 
 
-            if (this.batteryPct < 33) return "battery";
-            else if (this.batteryPct < 66) return "battery-half";
-            else return "battery-full";
+            if (this.batteryPct < 33) return "battery";                   // check if battery low return string warning "battery"
+            else if (this.batteryPct < 66) return "battery-half";         // if battery percent is ~half return string "battery half"
+            else return "battery-full";                                   // if battery percent is > 66% return "battery full"
         },
         batteryStyle() {
-            if (this.batteryPct == -1) return "`color: grey`"
-            if (this.batteryPct < 33) return `color: red`;
-            else if (this.batteryPct < 66) return `color: yellow`;
-            else return `color: green`;
+            if (this.batteryPct == -1) return "`color: grey`"             // if battery dead icon color is grey
+            if (this.batteryPct < 33) return `color: red`;                // if battery is low (< 33%) icon color is red
+            else if (this.batteryPct < 66) return `color: yellow`;        // if battery is ~half (33% < percent < 66%) icon color is yellow
+            else return `color: green`;                                   // if battery is > than 66% battery color is green
         },
     },
 };
@@ -35,19 +35,17 @@ export default {
 
 <style scoped>
 .batteryContainer {
-    overflow: auto;
-    padding: 10px 0px;
-}
-
+    overflow: auto;                                                       // if content is rendered outside battery icon element content is clipped and scroll bars are added if necessary by auto
+    padding: 10px 0px;                                                    // creates 10px of blank space on top and bottom inside battery icon and 0px space on left/right
 .battery {
-    width: 60px;
-    height: 38px;
-    margin-top: -8px;
-    float: left;
+    width: 60px;                                                          // width of battery icon
+    height: 38px;                                                         // height of icon
+    margin-top: -8px;                                                     // creates blank space of 8px around border of battery element 
+    float: left;                                                          // set the horizontal alignment of the battery icon
 }
 
 .batteryPercent {
-    float: left;
-    font-size: 11pt;
+    float: left;                                                          // set the horizontal alignment of the bat % #
+    font-size: 11pt;                                                      // font size of battery % #
 }
 </style>
