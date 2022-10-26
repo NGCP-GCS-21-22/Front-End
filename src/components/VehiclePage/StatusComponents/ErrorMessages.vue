c089418671af1da6559cfe201618a78036a866f7
AlecUrbany <98421122+AlecUrbany@users.noreply.github.com>
2022-10-26 13:28:31 -0700

    Updated ErrorMessages
    Added comments explaining the basics

--- src/components/VehiclePage/StatusComponents/ErrorMessages.vue
+++ src/components/VehiclePage/StatusComponents/ErrorMessages.vue
@@ -1,4 +1,5 @@
 <template>
+//Formatting stuff
     <b-container class="container">
         <h3>Error Messages</h3>
         <b-container
@@ -17,10 +18,12 @@
 </template>
 
 <script>
+//setting up the vehicle data for computing
 export default {
     props: {
         vehicleData: Object,
     },
+    //If the vehicle data does not return good data, an error message will be returned
     computed: {
         errorMessage() {
             if (!this.vehicleData) return "-----";
@@ -34,11 +37,13 @@ export default {
 h3 {
     text-decoration: underline;
 }
+//Creating the container for the message
 .container {
     padding: 15px 0px 0px 0px;
     height: 7vh;
     width: 100%;
 }
+//Formatting/Style for the message
 .messages {
     background: #f3f3f3;
     font-size: 15pt;
