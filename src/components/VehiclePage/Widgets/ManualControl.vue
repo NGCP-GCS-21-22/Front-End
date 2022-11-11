344f3de297ed4569c25f22a162e2d42d0e9ae9c5
AlecUrbany <98421122+AlecUrbany@users.noreply.github.com>
2022-11-11 13:59:12 -0800

    Update ManualControl.vue
    Comments fixed

--- src/components/VehiclePage/Widgets/ManualControl.vue
+++ src/components/VehiclePage/Widgets/ManualControl.vue
@@ -21,14 +21,14 @@
 import axios from "axios";
 
 export default {
-    //Different "object" names that will be used - name, VehicleName, and vehicleData.
+    <!-- //Different "object" names that will be used - name, VehicleName, and vehicleData. -->
     props: {
         name: String,
         vehicleName: String,
         vehicleData: Object,
     },
     computed: {
-        //If data is not null, return the mode the vehicle is in.
+        <!-- //If data is not null, return the mode the vehicle is in. -->
         mode() {
             if (!this.vehicleData) return null;
             return this.vehicleData.mode;
@@ -38,7 +38,7 @@ export default {
         goBack() {
             this.$emit("goBack");
         },
-        //Command to switch controls to manual. Will send a payload to the const path. That payload will switch the vehicle's mode
+        <!-- //Command to switch controls to manual. Will send a payload to the const path. That payload will switch the vehicle's mode -->
         switchToManual() {
             const path = "http://localhost:5000/send";
             let payload = {
@@ -61,7 +61,7 @@ export default {
     },
 };
 </script>
-//Design and style for the back button
+<!-- //Design and style for the back button -->
 <style scoped>
 .back-button {
     position: absolute;
@@ -81,7 +81,7 @@ export default {
     font-size: 30pt;
 }
 
-/* Position for the swith to manual button */
+<!-- /* Position for the swith to manual button */ -->
 .switch-to-manual {
     position: absolute;
     left: 50%;
