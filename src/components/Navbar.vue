93085e54046895e54f5d204242d9cee23574b03c
Kayla <85607915+kltang6328@users.noreply.github.com>
2022-11-11 15:34:08 -0800

    Added comments

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,14 @@
+<!-- Navbar at the top of the screen -->
 <template>
 	<div>
 		<b-navbar id="nav" toggleable="lg" type="dark">
+			<!-- Display NGCP logo -->
 			<b-img style="width: 75px; padding-left: 15px;" src="@/assets/ngcp.png"></b-img>
 			<div class="nav-title">Ground Control Station</div>
 			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 			<b-navbar-nav class="ml-auto">
 				<b-collapse id="nav-collapse" is-nav>
+					<!-- Display the links to other pages -->
 					<b-navbar-nav>
 						<b-nav-item :active="$route.name == 'Main'">
 							<b-link to="/gcs/main">Main</b-link>
@@ -40,7 +43,7 @@ export default defineComponent({});
 	padding: 8px;
 	width: 100%;
 	height: 8vh;
-}
+} /* set the background color and size */
 
 #nav a {
 	padding: 5px 20px;
@@ -48,16 +51,16 @@ export default defineComponent({});
 	color: rgb(226, 226, 226);
 	font-size: 25pt;
 	font-weight: 400;
-}
+} /* space the links apart and set the text's color and size */
 
 #nav a.router-link-exact-active {
 	color: #61a3ff;
 	text-decoration: underline;
-}
+} /* change color to blue when on that page */
 
 #nav a:hover {
 	color: rgb(144, 165, 209);
-}
+} /* change color to grey when hovering the link */
 
 .nav-title {
 	font-size: 18pt;
